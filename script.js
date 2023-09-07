5c05072e928d9a9b8f4cb7b232506826fccf856d
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-07 17:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let n=-1;
+
+	let list=document.getElementsByTagName("li");
+	
+	for(let i=0 ; i < list.length; i++){
+		if(list[i].id==="level"){
+			n=i;
+		}
+		
+	}
+	
+	window.onload = alert(n+1);
